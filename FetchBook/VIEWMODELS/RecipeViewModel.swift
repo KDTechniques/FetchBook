c4808246a1556cb5e8c7717336d04af5f174515d
Kavinda Dilshan <kdtechniques@gmail.com>
2024-10-05 04:45:10 +0530

    [Feature] add basic ui, and file structure

---
+++ FetchBook/VIEWMODELS/RecipeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeViewModel.swift
+//  FetchBook
+//
+//  Created by Mr. Kavinda Dilshan on 2024-10-05.
+//
+
+import Foundation
+
+final class RecipeViewModel: ObservableObject {
+    // MARK: PROPERTIES
+    
+    
+    
+    // MARK: - FUNCTIONS
+}
