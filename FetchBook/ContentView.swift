ff097319f73528ba31aa7c836073cb1f0464e466
Kavinda Dilshan <kdtechniques@gmail.com>
2024-10-05 04:00:31 +0530

    Initial Commit

---
+++ FetchBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchBook
+//
+//  Created by Mr. Kavinda Dilshan on 2024-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchBook/FetchBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchBookApp.swift
+//  FetchBook
+//
+//  Created by Mr. Kavinda Dilshan on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct FetchBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
