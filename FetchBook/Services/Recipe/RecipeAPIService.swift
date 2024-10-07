1f1ed0b04aa021c8a67aa7e25493cdae57154521
Kavinda Dilshan <kdtechniques@gmail.com>
2024-10-07 08:18:02 +0530

    Update RecipeAPIService.swift
    Resolved merge conflicts from the development branch

--- FetchBook/Services/Recipe/RecipeAPIService.swift
+++ FetchBook/Services/Recipe/RecipeAPIService.swift
@@ -14,14 +14,14 @@ class RecipeAPIService: RecipeDataFetching {
     // MARK: - fetchRecipeData
     /// Fetches recipe data from the specified API endpoint.
     ///
-    /// This asynchronous function retrieves recipe data from a given endpoint defined by the `RecipeViewModel.Endpoints` enum.
+    /// This asynchronous function retrieves recipe data from a given endpoint defined by the `RecipeEndpoints` enum.
     /// It constructs a URL from the endpoint, performs a network request, and decodes the response into a `RecipesModel` object.
     ///
-    /// - Parameter endpoint: The specific endpoint to fetch data from, defined by the `RecipeViewModel.Endpoints` enum.
+    /// - Parameter endpoint: The specific endpoint to fetch data from, defined by the `RecipeEndpointModel`.
     /// - Throws: `URLError` if the URL is invalid, or if the network request fails or the response cannot be decoded.
     /// - Returns: A `RecipesModel` containing the recipes fetched from the specified API endpoint.
-    func fetchRecipeData(from endpoint: RecipeViewModel.Endpoints) async throws -> RecipesModel {
-        let urlString = endpoint.rawValue
+    func fetchRecipeData(from endpoint: RecipeEndpointModel) async throws -> RecipesModel {
+        let urlString = endpoint.urlString
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
