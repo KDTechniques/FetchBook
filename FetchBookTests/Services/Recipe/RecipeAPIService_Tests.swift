0b4d94641f7eb97f8c009fec584536ab60cc9f21
Kavinda Dilshan <kdtechniques@gmail.com>
2024-11-01 04:13:42 +0530

    [Clean] change folder structure

---
+++ FetchBookTests/Services/Recipe/RecipeAPIService_Tests.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeAPIService_Tests.swift
+//  FetchBookTests
+//
+//  Created by Mr. Kavinda Dilshan on 2024-11-01.
+//
+
+import XCTest
+@testable import FetchBook
+
+@MainActor
+final class RecipeAPIService_Tests: XCTestCase {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: FUNCTIONS
+    
+    // MARK: - setUpWithError
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    // MARK: - tearDownWithError
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    // MARK: UNIT TESTS
+    
+    // MARK: -
+    
+}
